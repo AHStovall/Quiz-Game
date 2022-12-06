8babd3a1a451bb3a68d1ff2ad7667d4fc84de354
AHStovall <adamhstovall@gmail.com>
2022-12-05 23:56:04 -0500

    Refactored upon consultation with resources. Major updates on layout of JavaScript, implementing new functions and variables.

--- js/script.js
+++ js/script.js
@@ -1,78 +1,132 @@
 var timer;
 var timerCount;
+
 var quizBox = document.querySelector(".container");
 var answerBoxesEl = document.querySelector('.btn-layout');
+var startButton = document.querySelector("#btnStart");
+var continueButton = document.querySelector("#btnCont");
+var questionEl = document.querySelector('.question')
+var answerButtonEl = document.querySelector('.answer-buttons')
+
+
 var incorrect = "incorrect";
-var correct = "correct"; 
+var correct = "correct";
 var questionPrompt = {};
 var questionOption = {};
 var isWin = false;
-var startButton = document.querySelector("#btnStart");
+
 var scoreBoard = "#scoreBoard";
 
 
 startButton.addEventListener("click", startGame);
-
+continueButton.addEventListener('click', ()=> {
+    quizBox++;
+    presentNextQuestion();
+})
 
 var questionBank = [
-   questionOne = {
+    questionOne = {
         questionPrompt: "What does JSON stand for?",
         questionOption: [
-            {text: "JavaScript Object Notation", correct: true}, 
-            {text: "JQuery Storage Operating Notice", correct: false},
-            {text: "Joint System Operation Notification", correct: false},
-            {text: "JavaScript Object Notification", correct: false}],
+            { text: "JavaScript Object Notation", correct: true },
+            { text: "JQuery Storage Operating Notice", correct: false },
+            { text: "Joint System Operation Notification", correct: false },
+            { text: "JavaScript Object Notification", correct: false }],
     },
-   questionTwo = {
+    questionTwo = {
         questionPrompt: "The <a> tag is used for what in HTML?",
         questionOption: [
-            {text:"Large amounts of Text", correct: false}, 
-            {text:"Attributes", correct: false}, 
-            {text:"Accessbility Captions",  correct: false}, 
-            {text:"Anchors, or links", correct: true}],
-        
+            { text: "Large amounts of Text", correct: false },
+            { text: "Attributes", correct: false },
+            { text: "Accessbility Captions", correct: false },
+            { text: "Anchors, or links", correct: true }],
+
     },
-    questionThree={
+    questionThree = {
         questionPrompt: "How do you call a class in a CSS style sheet?",
         questionOption: [
-            {text: "#class", correct: false},
-            {text:".class", correct: true},
-            {text:"$class", correct: false},
-            {text:"!class",correct: false}],
-        
+            { text: "#class", correct: false },
+            { text: ".class", correct: true },
+            { text: "$class", correct: false },
+            { text: "!class", correct: false }],
+
     },
-    questionFour={
+    questionFour = {
         questionPrompt: "Which of these tags comes first on the page in HTML?",
         questionOption: [
-            {text:"<main>", correct: false},
-            {text:"<body>", correct: false},
-            {text:"<head>", correct: true},
-            {text:"h1",correct: false}],
-        
+            { text: "<main>", correct: false },
+            { text: "<body>", correct: false },
+            { text: "<head>", correct: true },
+            { text: "h1", correct: false }],
+
     }];
 
 
-function checkWin() {
-    var questionsRemaining = questionBank = 0;
-    if(questionBank.length === 0) {
-        endGame();
-    }
-    if(timerCount === 0){
-        endGame();
-    }
-};
+// function checkWin() {
+//     var questionsRemaining = questionBank = 0;
+//     if(questionBank.length === 0) {
+//         endGame();
+//     }
+//     if(timerCount === 0){
+//         endGame();
+//     }
+// };
 
-//Function for the end of the game, regardless of approach
-function endGame() {
-    return;
-}
+
 
 function presentQuestion() {
-    for (i=0; i<questionBank.length; i++) {
-    document.write(questionBank[0].questionPrompt = document.getElementById('#question1')),
-    questionBank[0].questionOption = document.getElementById('#question1'),
-    questionBank[0].questionCorrect = document.getElementById('#question1')
-}};
+    questionEl.innerText = quizBox[0].questionPrompt;
+    questionEl.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonEl.appendChild(button)
+    })
+}
+
+function presentNextQuestion() {
+    clearState()
+}
+
+function clearState() {
+    continueButton.classList.add('hide');
+    while(answerButtonEl.firstChild) {
+        answerButtonEl.removeChild(answerButtonEl.firstChild);
+    }
+}
+
+function selectAnswer() {
+    var selectedButton = e.target;
+    var correct = selectedButton.dataset.correct;
+    setClassStatus(document.body, correct);
+    Array.from(answerButtonEl.children).forEach(button => {
+        setClassStatus(button, button.dataset.correct)
+    });
+    if(quizBox.length !=0 && timerCount!=0) {
+    nextButton.classList.remove('hide');
+    } else {
+        scoreBoard.classList.remove('hide');
+    }
+}
+
+function setClassStatus(element, correct) {
+    clearClassStatus(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+
+    }
+}
+
+function clearClassStatus(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
 
 //Timer Function
 function startTimer() {
@@ -83,7 +137,8 @@ function startTimer() {
             clearInterval(timer);
             endGame();
         }
-    }, 1000);}
+    }, 1000);
+}
 
 // document.getElementById('wrong').addEventListener('click', function(){
 //     timer -=10;
@@ -91,7 +146,7 @@ function startTimer() {
 // });
 
 
-function startGame(){
+function startGame() {
     console.log("Started.");
     startButton.classList.add('hide');
     quizBox.classList.remove('hide');
@@ -99,3 +154,7 @@ function startGame(){
     presentQuestion();
 }
 
+//Function for the end of the game, regardless of approach
+function endGame() {
+    return;
+}
